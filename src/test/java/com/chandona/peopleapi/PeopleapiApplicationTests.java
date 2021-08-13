87bd4981c5b2d9fedcb644d3261e5604e4b38227
kupessa <loris.kupessa@gmail.com>
2021-08-13 17:44:09 +0100

    Primeiro commit

---
+++ src/test/java/com/chandona/peopleapi/PeopleapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandona.peopleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
